3990c10ffb08013d3bae7fc324222d690623051e
Truth-1 <philjustin73@gmail.com>
2026-04-29 11:02:34 +0100

    Switch Resource Group to data source

--- main.tf
+++ main.tf
@@ -1,35 +1,34 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+# We use 'data' instead of 'resource' because the RG already exists
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
 
 # Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "epic-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "epic-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
   name                = "epic-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = "epic-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -42,9 +41,9 @@ resource "azurerm_network_interface" "nic" {
 # Ubuntu VM
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "EpicBook-VM"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_D2s_v3"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
   admin_username      = var.vm_user
   network_interface_ids = [azurerm_network_interface.nic.id]
 
@@ -67,6 +66,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
 }
 
 # MySQL PaaS
+resource "random_id" "id" {
+  byte_length = 4
+}
+
 resource "random_password" "db_pass" {
   length  = 16
   special = true
@@ -74,21 +77,18 @@ resource "random_password" "db_pass" {
 
 resource "azurerm_mysql_flexible_server" "db" {
   name                   = "epicbook-db-${random_id.id.hex}"
-  resource_group_name    = azurerm_resource_group.rg.name
-  location               = azurerm_resource_group.rg.location
+  resource_group_name    = data.azurerm_resource_group.rg.name
+  location               = data.azurerm_resource_group.rg.location
   administrator_login    = "dbadmin"
   administrator_password = random_password.db_pass.result
   sku_name               = "B_Standard_B1ms"
+  version                = "8.0.21"
 }
 
 resource "azurerm_mysql_flexible_server_firewall_rule" "fw" {
   name                = "AllowAll"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   server_name         = azurerm_mysql_flexible_server.db.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "255.255.255.255"
-}
-
-resource "random_id" "id" {
-  byte_length = 4
 }
